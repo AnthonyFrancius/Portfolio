c78fc42c6ae642ff73b2d8ddc625ba4166289963
AnthonyFrancius <anthony.francius@oclock.school>
2025-08-25 18:45:16 +0200

    ajout des modals

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
 import linkedin from "./assets/linkedin-logo-linkedin-icon-transparent-free-png.webp";
 import github from "./assets/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png";
 import moi from "../public/moi.jpeg";
 
 function App() {
+  const [projet, setProjet] = useState(false);
+  const toggleProjet = () => {
+    setProjet(!projet);
+  };
+  const [diplome, setDiplome] = useState(false);
+  const toggleDiplome = () => {
+    setDiplome(!diplome);
+  };
+  const [savoir, setSavoir] = useState(false);
+  const toggleSavoir = () => {
+    setSavoir(!savoir);
+  };
+  const [exp, setExp] = useState(false);
+  const toggleExp = () => {
+    setExp(!exp);
+  };
+
   return (
     <div className="App">
       <header className="header">Anthony Francius</header>
@@ -24,7 +42,9 @@ function App() {
           <div className="ligne2">
             <div className="contact">
               <button>
-                <FiArrowUpRight />
+                <a href="mailto:anthony.francius@laposte.net">
+                  <FiArrowUpRight />
+                </a>
               </button>
               <h2>Contactez-moi</h2>
             </div>
@@ -43,28 +63,69 @@ function App() {
         <div className="droite">
           <div className="experience">
             <div className="projet">
-              <button>
+              <button onClick={toggleProjet}>
+                {projet && (
+                  <div className="modal">
+                    <div className="overlay">
+                      <div className="contenu">vive les chamois</div>
+                    </div>
+                  </div>
+                )}
                 <FiArrowUpRight />
               </button>
               <h4>Projet</h4>
             </div>
             <div className="trait" />
             <div className="savoir">
-              <button>
+              <button onClick={toggleSavoir}>
+                {savoir && (
+                  <div className="modal">
+                    <div className="overlay">
+                      <div className="contenu">aller l'om</div>
+                    </div>
+                  </div>
+                )}
                 <FiArrowUpRight />
               </button>
-              <h4>savoir faire</h4>
+              <h4>Savoir faire</h4>
             </div>
             <div className="trait" />
-            <div className="etude">
-              <button>
+            <div className="diplome">
+              <button onClick={toggleDiplome}>
+                {diplome && (
+                  <div className="modal">
+                    <div className="overlay" />
+                    <div className="contenu">
+                      <h1>Diplômes</h1>
+                      <h3>O'Clock</h3>
+                      <h4>En distenciel</h4>
+                      <p>Développeur web fullstack</p>
+                      <p>2024</p>
+                      <h3>Lycée St André</h3>
+                      <h4>Niort</h4>
+                      <p>Baccalauréal Négosiation et suivi de la clientèle</p>
+                      <p>2010</p>
+                      <h3>Lycée St André</h3>
+                      <h4>Niort</h4>
+                      <p>Brevet d'Etudes Professionnel comptabilité</p>
+                      <p>2008</p>
+                    </div>
+                  </div>
+                )}
                 <FiArrowUpRight />
               </button>
-              <h4>Etudes</h4>
+              <h4>Diplômes</h4>
             </div>
             <div className="trait" />
             <div className="exp">
-              <button>
+              <button onClick={toggleExp}>
+                {exp && (
+                  <div className="modal">
+                    <div className="overlay">
+                      <div className="contenu">nul psg</div>
+                    </div>
+                  </div>
+                )}
                 <FiArrowUpRight />
               </button>
               <h4>Expériences</h4>
